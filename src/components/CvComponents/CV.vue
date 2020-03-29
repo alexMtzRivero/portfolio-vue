cb3bc254a46eb706cc4c89aa58c1b265b42f3c04
alex <alejandromartinez@uvicate.com>
2020-03-28 19:44:57 -0700

    trabajos capvital y profesor

--- src/components/CvComponents/CV.vue
+++ src/components/CvComponents/CV.vue
@@ -1,12 +1,13 @@
 <template>
 <div class="wrap">
 
-<div>
+<div class='upBar'>
   <img class="flag" src="../../assets/france.png" alt="france flag" @click="changeInfo('french')">
   <img class="flag" src="../../assets/mexico.png" alt="mexico flag"  @click="changeInfo('spanish')">
   <img class="flag" src="../../assets/united-kingdom.png" alt="UK flag"  @click="changeInfo('english')">
+  <button @click="print()">Print</button>
 </div>
-  <div class="cv">
+  <div class='cv'>
     <!-- <button @click="print">print</button> -->
     <div id="presentation">
       <div class="intro">
@@ -31,29 +32,27 @@
         <div class="column">
           <h1>{{info.headers.profesionalExperience}}</h1>
           <div  v-for="job in info.profesionalExperience"  :key="job.tittle" class="default-list-item">
-            <h1 class="post">{{job.tittle}}</h1>
-            <h2 class="enterprise">{{job.enterprise}}</h2>
+            <h2 class="post">{{job.tittle}}</h2>
+            <h3 class="enterprise">{{job.enterprise}}</h3>
             <div class="date">{{job.timeBegin}}-{{job.timeEnd}}</div>
             <div class="place">{{job.place}}</div>
             <div class="description">{{job.description}}</div>
-            <div class="tecnologies">(tecnologies)</div>
+            <div v-for="skil in job.skils"  :key="skil" class="tecnologies">{{skil}}</div>
           </div> 
            <h1>{{info.headers.skills}}</h1>
           <ul>
             <li v-for="skill in info.skills" :key="skill">{{skill}}</li>
           </ul>
-           <h1>{{info.headers.education}}</h1>
-          <div  v-for="school in info.education"  :key="school.diploma" class="default-list-item">
-            <h2 class="diploma">{{school.diploma}}</h2>
-            <h3 class="school">{{school.school}}</h3>
-            <div class="date">{{school.timeBegin}}-{{school.timeEnd}}</div>
-          </div> 
+          <h1>{{info.headers.interest}}</h1>
+           <ul>
+            <li v-for="inter in info.interest" :key="inter">{{inter}}</li>
+          </ul>
         </div>
         <div class="column">
          
           <h1>{{info.headers.personalProjects}}</h1>
            <div  v-for="pp in info.personalProjects"  :key="pp.name" class="default-list-item">
-            <h1 class="tittle">{{pp.name}}</h1>
+            <h2 class="tittle">{{pp.name}}</h2>
             <div class="date">{{pp.timeBegin}}-{{pp.timeEnd}}</div>
             <div class="description">{{pp.description}}</div>
             <div class="tecnologies">{{pp.skills}}</div>
@@ -61,12 +60,14 @@
          
           <h1>{{info.headers.programingLenguages}}</h1>
           <ul>
-            <li v-for="pl in info.programingLenguages" :key="pl.name">{{pl.name}}--{{pl.level}}</li>
+            <li v-for="pl in info.programingLenguages" :key="pl.name">{{pl.name}}</li>
           </ul>
-          <h1>{{info.headers.interest}}</h1>
-           <ul>
-            <li v-for="inter in info.interest" :key="inter">{{inter}}</li>
-          </ul>
+          <h1>{{info.headers.education}}</h1>
+          <div  v-for="school in info.education"  :key="school.diploma" class="default-list-item">
+            <h2 class="diploma">{{school.diploma}}</h2>
+            <h3 class="school">{{school.school}}</h3>
+            <div class="date">{{school.timeBegin}}-{{school.timeEnd}}</div>
+          </div> 
           <h1>{{info.headers.achivements}}</h1>
           <ul>
             <li v-for="achivement in info.achivements" :key="achivement">{{achivement}}</li>
@@ -91,7 +92,8 @@ export default {
   },
   data(){
     return{
-      info:{"personalInformation":{}}
+      info:{"personalInformation":{}},
+      printing :false
     }
   },
   methods:{
@@ -104,7 +106,6 @@ export default {
   },
   // on create
   mounted(){
-    console.log(INFO);
     this.info = INFO.english;
   }
 }
@@ -119,7 +120,10 @@ export default {
 .wrap{
   width: 100%;
    background: #F2F2F2;
-  background: linear-gradient(33deg, rgb(100, 180, 166) 0%, #F2F2F2 100%); 
+  background: linear-gradient(33deg, #00D2E8 0%, #F2F2F2 50%); 
+}
+.wrap{
+  font-size: smaller;
 }
 .home{
 background: #F2F2F2
@@ -127,13 +131,24 @@ background: #F2F2F2
 .cv{
    background: #f2f2f2;
     color: #403433;
-   width: 600px;
+   width: 700px;
    margin:auto;  
 }
 
-#presentation{
+ button{
+   background-color: #0093E9;
+   color:#f2f2f2;
+   border-radius: 10px;
+   width: 4em;
+   height: 2em;
+   border: none;
+   position:absolute ;
+   font-size: medium;
+   margin: 1em;
+ }
+.cv #presentation{
   background: rgb(110,145,95);
-  background: linear-gradient(33deg, rgba(110,145,95,1) 0%, rgba(2,89,73,1) 100%); 
+  background: linear-gradient(33deg, #00D2E8 0%, #0093E9 100%); 
   color:#f2f2f2;
   display: flex;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
@@ -145,7 +160,7 @@ background: #F2F2F2
     padding: 1em;
     margin: 0 1em ;
 }
-img{
+.cv img{
   width: 50%;
   height: auto;
   margin: auto 0;
@@ -180,30 +195,75 @@ img{
    border-left: solid .2rem #403433;
 }
 #main h1{
-  color:#08A689;
+  color:#0093E9;
 }
 #main h2{
-   color:#08A689;
+   color:#00D2E8;
 }
 #main h3{
-  color: #403433;
+  color: #00D2E8;
 }
 ul{
   display:flexbox;
   padding-left: 0px;
 }
 #main li{
-  background:#08A689;
+  background:#0093E9;
   color: #F2F2F2;
-  margin: 10px;
+  margin: 5px;
   padding: 10px;
   border-radius: 10px;
   width: auto;
   display: inline-block;
 }
-@media screen and (max-width: 600px){
+@media print {
+  .upBar{
+  display: none;
+  }
+    .wrap{
+    font-size: smaller;
+  }
+  #presentation{
+    background-color: #0093E9 !important; 
+    -webkit-print-color-adjust: exact; 
+    color:#f2f2f2;
+    display: flex;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    padding: 0em 2em;
+  }
+  img{
+    width: 25%;
+    height: auto;
+    margin: auto 0;
+    border-radius: 50%;
+    border: solid .4rem rgba(0, 0, 0, 0.438);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  
+  }
+  #main h1{
+    font-size: medium;
+    color:#0093E9;
+  }
+  #main h2{
+      font-size: small;
+    color:#00D2E8;
+  }
+  #main h3{
+    font-size: smaller;
+    color: #00D2E8;
+  }
+
+  #main li{
+    color: #f2f2f2;
+    margin: 3px;
+    padding: 3px;
+    width: auto;
+    display: inline-block;
+    -webkit-print-color-adjust: exact; 
+  }
+}
+@media screen and (max-width: 700px){
   .cv{
-   
    width: 100%; 
 }
 
